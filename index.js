0023390fcefcbe5dc8bc9c484d090fb5274cefe6
JSS0124 <onlinebatool@gmail.com>
2025-07-18 16:58:27 +0500

    index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const serverless = require("serverless-http");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Import routes
+// Routes
 const categoryRoutes = require("./api/category");
 const deliveryRoutes = require("./api/deliveries");
 const deliverySingle = require("./api/delivery");
 const vendorRoutes = require("./api/vendors");
 const uploadRoutes = require("./api/uploadexcel");
 
-// Mount routes
 app.use("/api/categories", categoryRoutes);
 app.use("/api/deliveries", deliveryRoutes);
 app.use("/api/delivery", deliverySingle);
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/upload", uploadRoutes);
 
-// ✅ Export for Vercel (as handler function)
-module.exports = serverless(app);
+module.exports = app;
