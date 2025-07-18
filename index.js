05fd665aadd70cd328c7e72e6885b04d87355fce
JSS0124 <onlinebatool@gmail.com>
2025-07-18 18:07:58 +0500

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// API Routes
 const categoryRoutes = require("./api/category");
 const deliveryRoutes = require("./api/deliveries");
 const deliverySingle = require("./api/delivery");
@@ -17,5 +21,10 @@ app.use("/api/deliveries", deliveryRoutes);
 app.use("/api/delivery", deliverySingle);
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/upload", uploadRoutes);
+
+// Route root (/) to serve dashboard.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+});
 
 module.exports = app;
