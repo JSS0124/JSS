a20d7c7a586adf1b861bc40a60ab372d15ebddf0
JSS0124 <onlinebatool@gmail.com>
2025-08-04 17:49:21 +0500

    deliveries.js

--- api/deliveries.js
+++ api/deliveries.js
@@ -42,7 +42,7 @@ router.post('/add', async (req, res) => {
                 date,
                 notes
                 az_length
-            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+            ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
             RETURNING *`,
             [
                 customer_id,
