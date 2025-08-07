b6ac651c360bd3661fba60ce042a06270b2d06df
JSS0124 <onlinebatool@gmail.com>
2025-08-07 13:45:24 +0500

    db.js

--- db.js
+++ db.js
@@ -2,15 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.status(200).json({ time: result.rows[0].now });
-  } catch (error) {
-    console.error("❌ ERROR:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
+module.exports = pool;
