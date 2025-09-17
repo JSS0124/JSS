ca11a4c351d9fc4108fa573e8a4a007482c25777
JSS0124 <onlinebatool@gmail.com>
2025-09-17 16:55:10 +0500

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-// _db.js
-import { Pool } from 'pg';
-
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // or other config
-  ssl: { rejectUnauthorized: false } // for Neon
-});
